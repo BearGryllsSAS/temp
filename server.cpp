daab73229e64019c6f33d90ee9f9c2d2b0d9dbb7
BearGrylls <119752447+BearGryllsSAS@users.noreply.github.com>
2024-03-17 12:53:34 +0800

    更新 server.cpp

--- server.cpp
+++ server.cpp
@@ -5,14 +5,6 @@ server::server()
     // http_conn类对象
     users = new chat_conn[MAX_FD];
 
-    // root文件夹路径
-    char server_path[200];
-    getcwd(server_path, 200);           // 获取当前工作目录的绝对路径
-    char root[6] = "/root";
-    m_root = (char *)malloc(strlen(server_path) + strlen(root) + 1);
-    strcpy(m_root, server_path);
-    strcat(m_root, root);
-
     // 定时器
     users_timer = new client_data[MAX_FD];
 }
